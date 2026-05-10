202e49561d2fb89a282594e406496262c40d8578
Romu T. <r@tisserand.eu>
2026-05-10 15:53:25 +0200

    fixed breadcrumb

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,5 +1,5 @@
 export const ROUTES = {
-  home: { label: "Accueil", url: "/" },
-  pro: { label: "Projets professionnels", url: "/pro" },
-  perso: { label: "Projets personnels", url: "/perso" },
+  home: { name: "Accueil", url: "/" },
+  pro: { name: "Projets professionnels", url: "/pro" },
+  perso: { name: "Projets personnels", url: "/perso" },
 } as const;
