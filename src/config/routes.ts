b1f3a41562ded3a55a496e6ab00b67097ef698dd
Romu T. <r@tisserand.eu>
2026-05-10 15:32:35 +0200

    now uses routes

---
+++ src/config/routes.ts
@@ -0,0 +1,5 @@
+export const ROUTES = {
+  home: { label: "Accueil", url: "/" },
+  pro: { label: "Projets professionnels", url: "/pro" },
+  perso: { label: "Projets personnels", url: "/perso" },
+} as const;
